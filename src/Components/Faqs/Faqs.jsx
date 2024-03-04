0cd9f1cc874ff3b4a081e1b085bc04420cd214dc
Piyush Yadav <126597035+Piyushin04@users.noreply.github.com>
2024-03-05 01:04:29 +0530

    Update Faqs.jsx

--- src/Components/Faqs/Faqs.jsx
+++ src/Components/Faqs/Faqs.jsx
@@ -112,7 +112,7 @@ const Faqs = () => {
       <div className="bg-blue px-8 py-8 mb-12 rounded-md shadow-md">
         <AnimatedText
           text="Frequently Asked Questions"
-          className="!text-7xl !font-semibold !text-white !text-center !mb-16"
+          className="!text-5xl !text-white sm:!text-2xl font-semibold my-16 !text-center"
         />
         <div className="mt-20">
           <Faq data={data} styles={data.styles} config={config} />
